3ca28c544e0bc6316d571125994c0ab4d80c8a2c
Tom Stoepker <tom.stoepker@gmail.com>
2015-01-23 01:50:56 -0500

    converting LESS namespaces to SASS friendly mixin sytax

--- converter/converter.go
+++ converter/converter.go
@@ -16,6 +16,8 @@ type DataStream struct {
 
 var convertedFile string
 var stringBuffer bytes.Buffer
+var foundNameSpaces []string
+var nsCurlyCount int = 0
 
 func LessToSass(filename string) chan DataStream {
 	ch := make(chan DataStream)
@@ -43,6 +45,30 @@ func convert(file *os.File) string {
 }
 
 func swapSyntax(line string) string {
+	line = swapVars(line)
+	lessNameSpace := regexp.MustCompile("#(\\w|\\d|-|_)+\\s{")
+	openCurly := regexp.MustCompile("{")
+	closedCurly := regexp.MustCompile("}")
+	nameSpaces := lessNameSpace.FindAllString(line, -1)
+	if nameSpaces != nil {
+		foundNameSpaces = append(foundNameSpaces, strings.Join(nameSpaces, ", "))
+	}
+	if len(foundNameSpaces) > 0 {
+		if openCurly.MatchString(line) {
+			nsCurlyCount++
+		}
+		if closedCurly.MatchString(line) {
+			nsCurlyCount--
+		}
+	}
+	if nsCurlyCount == 0 {
+		foundNameSpaces = append(make([]string, 0))
+	}
+	line = swapMixins(line)
+	return line
+}
+
+func swapVars(line string) string {
 	variables := regexp.MustCompile("@")
 	cssReserved := regexp.MustCompile("\\$(media|import|keyframes|-webkit|-moz|-o)")
 	line = variables.ReplaceAllString(line, "$")
@@ -53,14 +79,34 @@ func swapSyntax(line string) string {
 			line = line[:ampersandIdx] + "@" + line[ampersandIdx+1:]
 		}
 	}
+	return line
+}
+
+func swapMixins(line string) string {
 	mixInDeclation := regexp.MustCompile(".(.)+\\((.)*\\)\\s{")
+	if !mixInDeclation.MatchString(line) {
+		return line
+	}
 	mixIns := mixInDeclation.FindAllStringSubmatchIndex(line, -1)
 	emptyParens := regexp.MustCompile("\\(\\)")
+	offByOneMixinConcat := regexp.MustCompile("-\\.")
+	var mixin string
+	if len(foundNameSpaces) > 0 && nsCurlyCount > 0 {
+		mixin = strings.Join(foundNameSpaces, "-")
+		hashtag := regexp.MustCompile("(#|{|\\s)")
+		//period := regexp.MustCompile("\\.")
+		mixin = hashtag.ReplaceAllString(mixin, "")
+		//mixin = period.ReplaceAllString(mixin, "-")
+		mixin = "@mixin " + mixin + "-"
+	} else {
+		mixin = "@mixin "
+	}
 	if len(mixIns) > 0 {
 		for i, _ := range mixIns {
 			idx := mixIns[i][0]
-			line = line[:idx] + "@mixin " + strings.Trim(line[idx+1:], " ")
+			line = line[:idx] + mixin + strings.Trim(line[idx+1:], " ")
 			line = emptyParens.ReplaceAllString(line, "")
+			line = offByOneMixinConcat.ReplaceAllString(line, "-")
 		}
 	}
 	return line
