6e931914fff2f618fd8633b965177f96f2891055
Tom Stoepker <tom.stoepker@gmail.com>
2015-02-13 22:25:48 -0500

    test coverage for key find and replace functions

---
+++ converter/converter_test.go
@@ -0,0 +1,69 @@
+package converter
+
+import (
+	"testing"
+)
+
+func TestSwapVars(t *testing.T) {
+	cases := []struct {
+		in, out string
+	}{
+		{".myMixin(@color, @size", ".myMixin($color, $size"},
+		{"@favColor: #333", "$favColor: #333"},
+		{"row@{index}", "row${index}"},
+	}
+
+	for _, c := range cases {
+		got := swapVars(c.in)
+		if got != c.out {
+			t.Errorf("swapVars should have converted %s to %s, but instead output %s", c.in, c.out, got)
+		}
+	}
+}
+
+func TestSwapMixins(t *testing.T) {
+	cases := []struct {
+		in, out string
+	}{
+		{".primary-text() {", "@mixin primary-text {"},
+		{".border-radius(@radius: 3px) {", "@mixin border-radius(@radius: 3px) {"},
+	}
+
+	for _, c := range cases {
+		got := swapMixins(c.in)
+		if got != c.out {
+			t.Errorf("swapMixins should have converted '%s' to '%s', but instead output '%s'", c.in, c.out, got)
+		}
+	}
+}
+
+func TestConvertStringMethods(t *testing.T) {
+	cases := []struct {
+		in, out string
+	}{
+		{"filter: ~\"alpha(opacity=@{opacity})\";", "filter: \"alpha(opacity=#{$opacity})\";"},
+	}
+
+	for _, c := range cases {
+		got := convertStringMethods(c.in)
+		if got != c.out {
+			t.Errorf("swapMixins should have converted '%s' to '%s', but instead output '%s'", c.in, c.out, got)
+		}
+	}
+}
+
+func TestConvertInterpolatedStrings(t *testing.T) {
+	cases := []struct {
+		in, out string
+	}{
+		{"row${idx}", "row#{$idx}"},
+		{"hello${world}", "hello#{$world}"},
+	}
+
+	for _, c := range cases {
+		got := convertInterpolatedStrings(c.in)
+		if got != c.out {
+			t.Errorf("swapMixins should have converted '%s' to '%s', but instead output '%s'", c.in, c.out, got)
+		}
+	}
+}
